e7978c1c184ebb394b21af24bc88804fd69ad932
IvanMonichev <risen15@yandex.ru>
2023-04-13 14:12:57 +0300

    style: update filters

--- project/src/store/site-data/selectors.ts
+++ project/src/store/site-data/selectors.ts
@@ -1,4 +1,4 @@
-import { StoreSlice, types } from '../../constant';
+import { StoreSlice } from '../../constant';
 import { State } from '../../types/state';
 import { Quest } from '../../types/types';
 import { createSelector } from '@reduxjs/toolkit';
@@ -7,7 +7,7 @@ import { getLevel, getType } from '../site-process/selectors';
 export const getIsQuestsLoading = ( { [StoreSlice.SiteData]: SITE_DATA }: State): boolean => SITE_DATA.isQuestsLoading;
 export const getQuests = ({ [StoreSlice.SiteData]: SITE_DATA }: State): Quest[] => SITE_DATA.quests;
 
-export const selectQuests = createSelector(
+/*export const selectQuests = createSelector(
   [getQuests, getType, getLevel],
   (quests, type, level) => quests
     .filter((quest) => {
@@ -22,4 +22,13 @@ export const selectQuests = createSelector(
       }
       return quest.level === level.toLowerCase();
     })
+);*/
+
+export const selectQuests = createSelector(
+  [getQuests, getType, getLevel],
+  (quests, type, level) => quests.filter((quest) => {
+    const typeParams = quest.type === type || type === 'all-quests';
+    const levelParams = quest.level === level.toLowerCase() || level === 'Any';
+    return typeParams && levelParams;
+  })
 );
